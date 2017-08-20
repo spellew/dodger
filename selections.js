4d51102cb4a3cb2b73683cfa2b2daf50c636b3ce
spellew <spellew1298@gmail.com>
2017-08-20 13:32:12 -0400

    Initial commit.

---
+++ selections.js
@@ -0,0 +1,8 @@
+
+function pokemon_js() {
+  window.open("https://spellewasc.github.io/portfolio/Pokemon_groupCodeAPI/groupCodeAPI.html");
+}
+
+function olympics_js() {
+  window.open("https://cfloresasc.github.io/pls/Olympic.html");
+}
